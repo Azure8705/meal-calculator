3f2e80291cb9c9681b51283313ddfa46ce6d797a
Azure <kvnliwag@gmail.com>
2015-08-15 16:05:57 -0700

    Initial Commit0

---
+++ Meal-Calculator/Meal-Calculator/README.md
@@ -0,0 +1,3 @@
+﻿# Meal-Calculator
+
+
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('mealCalc', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMaterial']);
+
+})();
