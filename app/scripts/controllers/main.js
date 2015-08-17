8316f36a81722cd5dbb8139c8a305266b48bbb28
Azure <kvnliwag@gmail.com>
2015-08-16 23:50:03 -0700

    Added Ordering list and completed total logic

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,4 +13,35 @@ angular.module('mealCalcApp')
           $scope.menu = data;
       });
 
+      $scope.myOrder = [];
+
+      $scope.myTotal = {
+          total: 0,
+          tax: 9,
+          tip: 15,
+          grandTotal: 0
+       };
+
+      $scope.grandTotalCalc = function() {
+          $scope.gtcTax = $scope.myTotal.total * ($scope.myTotal.tax / 100);
+          console.log($scope.gtcTax);
+          $scope.gtcTip = $scope.myTotal.total * ($scope.myTotal.tip / 100);
+          console.log($scope.gtcTip);
+          $scope.myTotal.grandTotal = $scope.myTotal.total + $scope.gtcTax + $scope.gtcTip;
+      };
+
+      $scope.addToOrder = function (newItem) {
+          $scope.myOrder.push(newItem);
+          $scope.myTotal.total += newItem.price;
+          $scope.grandTotalCalc();
+      };
+      $scope.removeFromOrder = function (item) {
+          var index = $scope.myOrder.indexOf(item);
+          $scope.myOrder.splice(index, 1);
+          $scope.myTotal.total -= item.price;
+          $scope.grandTotalCalc();
+      };
+
+      
+      
   });
